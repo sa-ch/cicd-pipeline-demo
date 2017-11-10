fe0d57cbfeefeb8087a4eff8aafcd08c9d4cc8f3
eberlec <ceberle@redhat.com>
2017-11-10 11:20:08 +0100

    Update jenkins.sh

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -1,4 +1,8 @@
-oc new-project $LOGNAME-jenkins --display-name "Shared Jenkins"
+#!/bin/bash
+
+./init.sh
+
+oc new-project ${LOGNAME}-jenkins --display-name "Shared Jenkins"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
 
 # TODO: adapt slave pod config to have 2Gi mem
