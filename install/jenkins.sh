36a20f185d75cb8416f717f3a83bb307dd9c9b95
eberlec <ceberle@redhat.com>
2017-11-10 14:15:49 +0100

    patch pvc definition

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -15,4 +15,11 @@ fi
 oc new-project ${LOGNAME}-cicd-pipeline-demo-jenkins --display-name "Shared Jenkins"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
 
+oc rollout pause dc jenkins
+oc patch pvc jenkins -p "metadata:
+  name: jenkins
+  annotations:
+    volume.beta.kubernetes.io/storage-class: gluster-container"
+oc rollout resume dc jenkins
+
 # TODO: adapt slave pod config to have 2Gi mem
