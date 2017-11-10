68a19551267765f1e7d7d93baabcf6ddf8b27c64
Sacha Dubois <sadubois@redhat.com>
2017-11-10 14:12:20 +0100

    added

--- install/init.sh
+++ install/init.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-LOGNAME=$(oc whoami)
-
-#oc new-project ${LOGNAME}-cicd-pipeline-demo-nexus
-
 projectExist () {  
  oc get project $1 > /dev/null 2>&1
  echo $?
 }
 
-if [ $(projectExist ${LOGNAME}-cicd-pipeline-demo-nexus) == 1 ]; then 
-  oc new-project ${LOGNAME}-cicd-pipeline-demo-nexus
-else
-  echo gugu
-fi
-
+LOGNAME=$(oc whoami > /dev/null 2>&1)
--- install/jenkins.sh
+++ install/jenkins.sh
@@ -2,6 +2,10 @@
 
 ./init.sh
 
+if [ "${LOGNMAME}" != "" ]; then
+  echo "du dubeli, sätsch ilogge"
+fi
+
 if [ $(projectExist ${LOGNAME}-cicd-pipeline-demo-jenkins) == 1 ]; then
   echo "project for jenkins already exists - skipping setup"
   exit
